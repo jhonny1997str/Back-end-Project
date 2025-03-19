00b1ea38bcffc009a4fc56a3436bcd805df473fe
jhonny1997str <167597745+jhonny1997str@users.noreply.github.com>
2025-03-19 13:51:30 -0500

    Testing

--- src/main/java/com/futbolista/FutbolistaApiApplication.java
+++ src/main/java/com/futbolista/FutbolistaApiApplication.java
@@ -9,13 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FutbolistaApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 
-		// Cargar las variables de entorno en el sistema
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		SpringApplication.run(FutbolistaApiApplication.class, args);
 		Log.info("Carga completa, sigamos trabajando!!");
 	}
